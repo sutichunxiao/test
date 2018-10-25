9ea2184101373c6789029e16d5ddf67f85f89398
sutichunxiao <15586784538@163.com>
2018-10-25 13:53:11 +0800

    test.h

--- test.c
+++ test.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
- int main(int argc,char* argv)
+
+__stdcall int fun(int x,int y)
+{
+	return x+y;
+}
+
+
+int main(int argc,char* argv)
 {
 	printf("this is a git demo!\n");
 }
--- test.h
+++ test.h
@@ -1,4 +1,4 @@
 #ifndef TEST_H_
 #define TEST_H_
-
+int fun(int x,int y);
 #endif
